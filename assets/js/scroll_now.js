55fb8f6edcd37525e3cba0d51fe14f451036bb2c
Alex Agranov <alex.agranov@audiocodes.com>
2021-11-07 08:44:17 +0200

    scroll to current time

---
+++ assets/js/scroll_now.js
@@ -0,0 +1,38 @@
+$(function() {
+  let now = new Date();
+  let nowDate = now.getFullYear() + '-' + ('0' + (now.getMonth()+1)).slice(-2) + '-' + ('0' + now.getDate()).slice(-2);
+  $('.day-filter').each(function() {
+    if ($(this).hasClass(nowDate)) {
+      $('#now-button').show();
+      dateFound = true;
+    }
+  });
+
+  $('#now-button').click(function() {
+    let now = new Date();
+    let nowDate = now.getFullYear() + '-' + ('0' + (now.getMonth()+1)).slice(-2) + '-' + ('0' + now.getDate()).slice(-2);
+    let nowTime = ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2);
+    let elem;
+
+    $('.day-filter').each(function() {
+      if ($(this).hasClass(nowDate)) {
+        $(this).find('.time-filter').each(function() {
+          let eventTime = $(this).find('.eventtime').eq(0).find('h4').eq(0).text();
+          if (eventTime < nowTime) {
+            elem = $(this);
+          } else {
+            if (!elem) {
+              elem = $(this);
+            }
+            return false;
+          }
+        });
+        return false;
+      }
+    });
+
+    if (elem) {
+      $('html, body').animate({scrollTop: elem.offset().top - 100}, 1000);
+    }
+  });
+});
